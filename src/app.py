a75388296adc307f4b5d863c56d47bccc8b54d6d
AhmedThabet11 <ahmed111first@gmail.com>
2026-06-04 00:01:00 +0200

    Added reflected XSS vulnerability demonstration

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from src.routes.landing_pages_routes import admin_dash_bp, home_bp, contact_bp, cart_bp
 from src.routes.product_routes import product_bp
 from src.routes.auth_routes import login_bp, register_bp
-
+from src.routes.vulnerability_routes import vulnerability_bp
 
 
 def create_app():
@@ -16,5 +16,6 @@ def create_app():
     app.register_blueprint(cart_bp)
     app.register_blueprint(login_bp)
     app.register_blueprint(register_bp)
+    app.register_blueprint(vulnerability_bp)
 
     return  app
