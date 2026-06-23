6d6fef89aa0eff5c25efe8105e970cb0b3f8cc2e
kalil sacko <kalil.sacko.3@gmail.com>
2026-06-23 03:39:47 +0200

    some adjustment

--- src/services/profile_service.py
+++ src/services/profile_service.py
@@ -1,8 +1,11 @@
 from src.services.cart_service import get_cart_by_user
+from src.services.order_service import get_orders_by_user
 
 
 def get_profile_cart_stats(user_id):
+
     cart_items = get_cart_by_user(user_id)
+    orders = get_orders_by_user(user_id)
 
     cart_items_count = 0
     cart_total = 0
@@ -11,7 +14,10 @@ def get_profile_cart_stats(user_id):
         cart_items_count += item["quantity"]
         cart_total += item["total_price"]
 
+    order_count = len(orders)
+
     return {
         "cart_items_count": cart_items_count,
-        "cart_total": cart_total
+        "cart_total": cart_total,
+        "order_count": order_count
     }
